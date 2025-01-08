42f947b0997fa2cc7b25d37f254c359239adf2d8
Antonio Molinari <molinari@incode.it>
2025-01-08 13:03:46 +0100

    add data.json for project details; update App.vue to load data and implement shuffle functionality; add reference to README

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,46 @@
 <script setup>
+
+import { ref, onMounted } from 'vue';
 import Navbar from './components/Navbar.vue';
 import Intro from './components/Intro.vue';
 import Gallery from './components/Gallery.vue';
 
-const items = [
+import data from '../public/data.json';
 
-  {
-    id: 1,
-    title: 'Project 1',
-    description: 'A comprehensive project that explores advanced CSS techniques and responsive design principles.',
-    img: 'https://via.placeholder.com/640x640'
-  },
-  {
-    id: 2,
-    title: 'Project 2',
-    description: 'An innovative web application that leverages modern JavaScript frameworks for dynamic user experiences.',
-    img: 'https://via.placeholder.com/640x640'
-  },
-  {
-    id: 3,
-    title: 'Project 3',
-    description: 'A detailed case study on the implementation of RESTful APIs and backend services.',
-    img: 'https://via.placeholder.com/640x640'
-  },
-  {
-    id: 4,
-    title: 'Project 4',
-    description: 'A creative portfolio showcasing the integration of multimedia elements and interactive features.',
-    img: 'https://via.placeholder.com/640x640'
-  },
-  {
-    id: 5,
-    title: 'Project 5',
-    description: 'A creative portfolio showcasing the integration of multimedia elements and interactive features.',
-    img: 'https://via.placeholder.com/640x640'
-  },
-]
+const items = ref([]);
+
+onMounted(() => {
+  console.log("gallery component mounted");
+  items.value = data;
+});
+
+
+const shuffle = (array) => { 
+  for (let i = array.length - 1; i > 0; i--) { 
+    const j = Math.floor(Math.random() * (i + 1)); 
+    [array[i], array[j]] = [array[j], array[i]]; 
+  } 
+  return array; 
+}; 
+
+
+const handleClick = () => {
+  //items.value.shift();
+  items.value = shuffle(items.value);
+  console.log("button clicked");
+}
+
+
 
 </script>
 
 
-<!-- reference https://michieldegraaf.com/ -->
+
 <template>
   <div class="container">
     <Navbar />
     <Intro />
+    <a class="button" @click="handleClick">button</a>
     <Gallery :items="items" />
   </div>
 </template>
